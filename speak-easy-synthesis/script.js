9bf4cd07a0b7094c4d91f9fa011676dc249613f7
Spejderman <r.odgaard@gmail.com>
2019-10-21 16:21:10 +0200

    Finished commenting the code. Added new title. Included txt containng information on the different voices and how they should be treated

--- speak-easy-synthesis/script.js
+++ speak-easy-synthesis/script.js
@@ -6,23 +6,24 @@ var inputForm = document.querySelector('form');
 var inputTxt = document.querySelector('.txt');
 var voiceSelect = document.querySelector('select');
 
+// Slider object for pitch and rate
 var pitch = document.querySelector('#pitch');
 var rate = document.querySelector('#rate');
 
-
+// Output value from sliders for pitch and rate
 var pitchValue = document.querySelector('.pitch-value');
 var rateValue = document.querySelector('.rate-value');
 
 // all the different voices loaded from the API
 var voices = [];
 
+// Here all the text inputs from Fred should be saved in an array-like format
 var introText = 'Whats up gangsta. Welcome to Googles hiphop beat service. I am here to make your rhymes sound tight. First you have to choose a pattern. Do you want simple, funky, or complex?';
 //var introText = 'bum ti bum bum ti bum ti bum bum ti bum ti bum bum ti bum ti bum bum ti bum ti bum bum ti ';
 
 
-
-
-//Function for retrieveing all available voices and their names
+// Function for retrieveing all available voices and their names
+// Don't meddle with this, just use it!
 function populateVoiceList() {
   voices = synth.getVoices().sort(function (a, b) {
       const aname = a.name.toUpperCase(), bname = b.name.toUpperCase();
@@ -54,6 +55,7 @@ if (speechSynthesis.onvoiceschanged !== undefined) {
   speechSynthesis.onvoiceschanged = populateVoiceList;
 }
 
+// Original speak function - Sets the wheels in motion
 function speak(){
     if (synth.speaking) {
         console.error('speechSynthesis.speaking');
@@ -80,6 +82,7 @@ function speak(){
   }
 }
 
+// Speak function overload with string input parameter
 function speak(input)
 {
   if (synth.speaking) {
@@ -109,20 +112,23 @@ function speak(input)
   }
 }
 
+// Event handler for when the form is submitted to the web API
 inputForm.onsubmit = function(event) {
   event.preventDefault();
-
   speak(introText);
 }
 
+// Event handler for when the pitch slider have been changed
 pitch.onchange = function() {
   pitchValue.textContent = pitch.value;
 }
 
+// Event handler for when the rate slider have been changed
 rate.onchange = function() {
   rateValue.textContent = rate.value;
 }
 
+// Event handler for when the voice drop down have been changed
 voiceSelect.onchange = function(){
   speak();
 }
